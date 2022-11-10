feb3755d4bac435bc86fb1ff98cacc63f0902a04
Anson Tang <atang@finverse.com>
2022-11-10 10:03:17 +0800

    add ios example app

---
+++ ios-demo-app/ios-demo-app/ios_demo_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  ios_demo_appApp.swift
+//  ios-demo-app
+//
+//  Created by Anson Tang on 9/11/2022.
+//
+
+import SwiftUI
+
+@main
+struct ios_demo_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
